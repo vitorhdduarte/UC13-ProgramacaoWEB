d7ff8c558da12b4a467a64155ba84993075d9000
vitor henrique <vitor.hdduarte@senacsp.edu.br>
2023-08-29 17:00:51 -0300

    Aula-Bootstrap

---
+++ index.php
@@ -0,0 +1,102 @@
+<html lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport">
+
+<head>
+    <title>
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+
+    <div class="container text-bg-primary p-1 m-2 text-center  rounded-5 border border-warning">
+
+     <h1>aula de bootstrap</h1>
+
+     </div>
+
+      <div class="row">
+
+        <div class="bg-danger col-12 col-md-4 col-xl-12">
+
+            <h2>coluna 1</h2>
+
+            </div>
+
+            <div class="bg-warning col-12 col-md-4">
+
+            <h2>coluna 2</h2>
+
+            </div>
+
+            <div class="bg-secondary col-12 col-md-4 d-none d-md-block">
+
+            <h2>coluna 3</h2>
+
+            </div>
+
+ 
+
+      </div>
+
+      <div class="row">
+
+        <div class="bg-danger col-12 col-md-4 col-xl-12">
+
+            <h2>Bootstrap</h2>
+
+            </div>
+
+            <div class="bg-warning col-4 col-md-12">
+
+            <h2>coluna 1</h2>
+
+            </div>
+
+            <div class="bg-secondary col-4 col-md-6">
+
+            <h2>coluna 2</h2>
+
+            </div>
+
+            <div class="bg-info col-4 col-md-6">
+
+            <h2>coluna 3</h2>
+
+            </div>
+
+            <div class="bg-primary col-12 col-md-12">
+
+            <h2>coluna 4</h2>
+
+            </div>
+
+            <div class="bg-warning col-6 col-md-12">
+
+            <h2>coluna 5</h2>
+
+            </div>
+
+            <div class="bg-danger col-6 col-md-12 d-none d-md-block">
+
+            <h2>coluna 6</h2>
+
+            </div>
+
+            <div class="bg-secondary col-12 col-md-12">
+
+            <h2>coluna 7</h2>
+
+            </div>
+
+            </div>
+
+
+
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
